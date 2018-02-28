6169708d2925c2b97f44db1c99bad2ded428fa79
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-02-28 12:26:54 +0900

    fix page-routing

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 
 from pages import application
 
+app = application
+
+
 if __name__ == '__main__':
     application.run(host='localhost', port=8080, debug=True)
--- pages/__init__.py
+++ pages/__init__.py
@@ -1,10 +1,19 @@
 # coding: utf-8
 
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 import index
 import subpage
 
 application = Flask(__name__)
+
+
+@application.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
 
 modules_define = [index.app, subpage.app]
 for app in modules_define:
